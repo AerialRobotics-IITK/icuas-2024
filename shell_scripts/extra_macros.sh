3525a189f38eeb7c701da3ba638d1050311799d7
Pulak Gautam <pulak.gautam@gmail.com>
2024-02-08 16:38:24 +0530

    added timer shell script

--- shell_scripts/extra_macros.sh
+++ shell_scripts/extra_macros.sh
@@ -5,5 +5,25 @@ waitForPlants() {
     sleep 1
   done
   echo "All plant beds spawned!"
-  sleep 1
+}
+
+waitForCount() {
+  until rostopic list | grep -q "/red/fruit_count"; do
+    sleep 1
+  done
+  echo "Fruit Count generated"
+}
+
+TimeEvent() {
+  waitForPlants
+  start=`date +%s.%N`
+  waitForCount
+  end=`date +%s.%N`
+
+  runtime=$( echo "($end - $start)" | bc -l) 
+
+  runtime=$( printf %d $runtime 2> /dev/null ) 
+
+  echo "Elapsed: $(($runtime / 60))min $(($runtime % 60))sec"
+
 }
