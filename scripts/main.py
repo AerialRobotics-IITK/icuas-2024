b63c9a0c8f1c5d34f38294b94d46e87b1c5ff555
Pulak Gautam <pulak.gautam@gmail.com>
2024-02-23 23:51:40 +0530

    added color coding for coords

--- scripts/main.py
+++ scripts/main.py
@@ -228,9 +228,8 @@ if __name__ == "__main__":
                 for fruit in detected_fruits:
                     if fruit[3] == plant_type:
                             #Change the values of threshold here
-                        if append_fruits(fruit_positions, [fruit[0], fruit[1], fruit[2]], threshold_x = 0.1, threshold_2d = 0.1):
-                        
-                            print(f"INFO: Detected fruit at ({fruit[0]}, {fruit[1]}, {fruit[2]}) added to count")
+                        if append_fruits(fruit_positions, [fruit[0], fruit[1], fruit[2]], threshold_x = 0.7, threshold_2d = 0.1):
+                            print(f"{bcolors.OKBLUE}INFO: Detected fruit at ({fruit[0]}, {fruit[1]}, {fruit[2]}) added to count {bcolors.ENDC}")
                         else: 
                             print(f"{bcolors.WARNING}WARNING: Skipping detected fruit at ({fruit[0]}, {fruit[1]}, {fruit[2]}) to avoid double counting! {bcolors.ENDC}")
                     else:
